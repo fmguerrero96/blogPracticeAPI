018ffbf22a1b12f1cdda7606789dc62657ec99dd
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-19 13:09:04 -0600

    Connect to mongo database and create simple endpoint

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect('mongodb+srv://mguerrerowest:DAu6D7LcgrnAGvoA@cluster0.cqwis4a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+app.post('/register', (req,res) => {
+    const {username, password} = req.body
+    res.json({requestData: {username, password}})
+});
 
 app.listen(4000)
