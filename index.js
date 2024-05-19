796e6996da643a81ba6369ddf0200a186bf58f73
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-19 15:16:23 -0600

    Connect to mongo db and import User model

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const User = require('./models/User')
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://mguerrerowest:DAu6D7LcgrnAGvoA@cluster0.cqwis4a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-
-app.post('/register', (req,res) => {
+mongoose.connect('mongodb+srv://mguerrerowest:mypassword@cluster0.cqwis4a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  
+app.post('/register', async (req,res) => {
     const {username, password} = req.body
-    res.json({requestData: {username, password}})
+    const userDoc = await User.create({username, password}) 
+    res.json(userDoc)
 });
 
 app.listen(4000)
