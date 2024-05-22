c7c7352e8d36f683f685bf244dd4296667d18b3b
Misael Guerrero <mguerrerowest@gmail.com>
2024-05-22 17:06:58 -0600

    Create endpoint for sending all posts in db on GET

--- index.js
+++ index.js
@@ -90,4 +90,9 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
     res.json(postDoc)
 });
 
+app.get('/post', async (req, res) => {
+    const posts = await Post.find()
+    res.json(posts)
+});
+
 app.listen(4000)
